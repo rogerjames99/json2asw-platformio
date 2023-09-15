ef468476f0c17f1340ed419ca69995d1b9e27cc2
Roger James <roger@beardandsandals.co.uk>
2023-09-15 15:55:21 +0100

    Last commit was aload of rubbish

--- main.cpp
+++ main.cpp
@@ -14,15 +14,17 @@ struct my_instrument_data : public instrument_data
 void my_instrument_data::serialize()
 {
     std::size_t sample_count_size = sizeof sample_count;
-    std::size_t sample_note_ranges_size = sizeof sample_note_ranges;
-    std::size_t samples_size = sizeof samples;
+    std::size_t sample_note_ranges_size = sizeof *sample_note_ranges;
+    std::size_t samples_size = sizeof *samples;
     std::ofstream ostrm("irish2.dump", std::ios::binary);
-    ostrm.write(reinterpret_cast<char*>(&sample_count_size), sizeof sample_count_size);
-    ostrm.write(reinterpret_cast<char*>(&sample_note_ranges_size), sizeof sample_note_ranges);
-    ostrm.write(reinterpret_cast<char*>(&samples_size), sizeof samples_size);
-    ostrm.write(reinterpret_cast<char*>(&sample_count), sizeof sample_count);
-    ostrm.write(reinterpret_cast<char*>(&sample_note_ranges), sizeof sample_note_ranges);
-    ostrm.write(reinterpret_cast<char*>(&samples), sizeof samples);
+    /* This is a load of rubbish
+    ostrm.write(reinterpret_cast<char*>(sample_count_size), sizeof sample_count_size);
+    ostrm.write(reinterpret_cast<char*>(sample_note_ranges_size), sizeof sample_note_ranges);
+    ostrm.write(reinterpret_cast<char*>(samples_size), sizeof samples_size);
+    ostrm.write(reinterpret_cast<char*>(sample_count), sizeof sample_count);
+    ostrm.write(reinterpret_cast<char*>(sample_note_ranges), sizeof sample_note_ranges);
+    ostrm.write(reinterpret_cast<char*>(samples), sizeof samples);
+    */
 }
 
 int main(int, char**)
