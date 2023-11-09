278ca16119a0b9555e26fcb489615292339ac241
Roger James <roger@beardandsandals.co.uk>
2023-11-09 16:22:01 +0000

    Added instrument class

---
+++ platformio/src/instrument.cpp
@@ -0,0 +1,225 @@
+/** \file instrument.cpp
+*/
+#include <ArduinoLog.h>
+#undef CR
+#include <SD.h>
+#include "instrument.h"
+static const int chipSelect = BUILTIN_SDCARD;
+
+char filePosition[10];
+
+CInstrument* CInstrument::singleton = nullptr;
+
+CInstrument* CInstrument::getInstance()
+{
+    if (singleton == nullptr)
+        singleton = new CInstrument();
+    return singleton;
+}
+
+void CInstrument::updateFilePosition(File* file)
+{
+    snprintf(filePosition, 10, "%llu", file->position());
+}
+
+void CInstrument::dumpHexBytes(uint8_t *bytes, size_t count)
+{
+    char row[128];
+    size_t index = count - (count % 8);
+    Log.verbose("Line %d bytes %x count %d index %d\n", __LINE__, bytes, count, index);
+    size_t i = 0;
+    for (; i < index; i = i + 8 )
+    {
+        //Log.verbose("Line %d i %d\n", __LINE__, i);
+        snprintf(row, 512, "%x %02x %02x %02x %02x %02x %02x %02x %02x\n",
+            i, bytes[i], bytes[i + 1], bytes[i + 2], bytes[i + 3],
+            bytes[i + 4], bytes[i + 5], bytes[i + 6], bytes[i + 7]);
+        Log.verbose("Line %d row %s",__LINE__, row);
+    }
+
+    if (index != count)
+    {
+        char * row_ptr = row;
+        for (; i < count; i++)
+        {
+            //Log.verbose("Line %d i %d\n", __LINE__, i);
+            row_ptr = row_ptr + snprintf(row_ptr, 512, "%02x ", bytes[i]);
+        }
+        Log.verbose("Line %d row %s\n", __LINE__, row);
+    }
+}
+
+void CInstrument::dumpInstrumentData(AudioSynthWavetable::instrument_data *instrumentData)
+{
+        char buf[128];
+        snprintf(buf, 128, "Instrument data\n"
+                            "===============\n"
+                            "Number of samples %d"
+                            "Sample note ranges pointer %p\n",
+                            instrumentData->sample_count,
+                            instrumentData->sample_note_ranges
+                            );
+        Log.verbose("%s", buf);
+}
+
+struct AudioSynthWavetable::instrument_data* CInstrument::load(const char *name)
+{
+
+    if (!SD.begin(chipSelect))
+    {
+        Log.verbose("Line %d SD card intialisation failed\n", __LINE__);
+        return nullptr;
+    }
+
+    if (!SD.exists(name))
+    {
+        Log.verbose("Line %d Cannot find file\n", __LINE__);
+        return nullptr;
+    }
+    
+    File data = SD.open(name);
+    if (!data)
+    {
+        Log.verbose("Line %d Unable to open data file\n", __LINE__);
+        return nullptr;
+    }
+
+    // 1. Read the number of samples (uint8_t)
+    Log.verbose("Line %d sizeof(uint8_t) %d sizeof instrument_data.sample_count %d\n", __LINE__, sizeof(uint8_t), sizeof instrument_data.sample_count);
+    if (data.read(&instrument_data.sample_count, sizeof instrument_data.sample_count) != sizeof instrument_data.sample_count)
+    {
+        Log.verbose("Failed to read number of samples\n", __LINE__);
+        data.close();
+        return nullptr;
+    }
+    Log.verbose("Line %d Number of samples %d\n", __LINE__, instrument_data.sample_count);
+    updateFilePosition(&data);
+    Log.verbose("Line %d current file position %s\n", __LINE__, filePosition);
+
+    // 2. Allocate memory to hold the raw sample sizes.
+    if (nullptr != raw_sample_sizes)
+    {
+        free(raw_sample_sizes);
+        raw_sample_sizes = nullptr;
+    }
+    
+    size_t raw_sample_sizes_size = sizeof(uint16_t) * instrument_data.sample_count;
+    Log.verbose("Line %d raw_sample_sizes %d\n", __LINE__, raw_sample_sizes_size);
+    if (nullptr == (raw_sample_sizes = (uint16_t*)malloc(raw_sample_sizes_size)))
+    {
+        Log.verbose("Line %d Failed to allocate memory for raw_sample_sizes\n", __LINE__);
+        data.close();
+        return nullptr;
+    }
+
+    // 3. Read the raw sample sizes.
+    if (data.read(raw_sample_sizes, raw_sample_sizes_size) != raw_sample_sizes_size)
+    {
+        Log.verbose("Line %d Failed to read raw sample sizes\n", __LINE__);
+        data.close();
+        return nullptr;
+    }
+    updateFilePosition(&data);
+    Log.verbose("Line %d current file position %s\n", __LINE__, filePosition);
+
+
+    // 4. Allocate memory to hold the per sample note ranges.
+    size_t sample_note_ranges_array_size = sizeof(uint8_t) * instrument_data.sample_count;
+    Log.verbose("Line %d sample_note_ranges_array_size %d\n", __LINE__, sample_note_ranges_array_size);
+    if (nullptr != sample_note_ranges_array)
+    {
+        free(sample_note_ranges_array);
+        sample_note_ranges_array = nullptr;
+    }
+
+    if (nullptr == (sample_note_ranges_array = (uint8_t*)malloc(sample_note_ranges_array_size)))
+    {
+        Log.verbose("Line %d Failed to allocate memory for sample_note_ranges_array\n", __LINE__);
+        data.close();
+        return nullptr;
+    }
+    instrument_data.sample_note_ranges = sample_note_ranges_array;
+
+    // 5. Read the sample_note_ranges.
+    if (data.read(sample_note_ranges_array, sample_note_ranges_array_size) != sample_note_ranges_array_size)
+    {
+        Log.verbose("Line %d Failed to read sample sample note ranges\n", __LINE__);
+        data.close();
+        return nullptr;
+    }
+    updateFilePosition(&data);
+    Log.verbose("Line %d current file position %s\n", __LINE__, filePosition);
+
+    // 6. Allocate memory for the sample metadata.
+    size_t samples_metadata_array_size = ((sizeof(AudioSynthWavetable::sample_data)) * instrument_data.sample_count);
+    if (nullptr != samples_metadata_array)
+    {
+        free(samples_metadata_array);
+        samples_metadata_array = nullptr;
+    }
+
+    if (nullptr == (samples_metadata_array = (AudioSynthWavetable::sample_data*)malloc(samples_metadata_array_size)))
+    {
+        Log.verbose("Line %d Failed to allocate memory for samples_metadata_array\n", __LINE__);
+        free(sample_note_ranges_array);
+        data.close();
+        return nullptr;
+    }
+    instrument_data.samples = samples_metadata_array;
+
+    // 7. Read the sample metadata.
+    if (data.read(samples_metadata_array, samples_metadata_array_size) != samples_metadata_array_size)
+    {
+        Log.verbose("Line %d Failed to read samples metadata\n", __LINE__);
+        data.close();
+        free(sample_note_ranges_array);
+        free(samples_metadata_array);
+        return nullptr;
+    }
+    updateFilePosition(&data);
+    Log.verbose("Line %d current file position %s\n", __LINE__, filePosition);
+
+
+    // 8. For each sample metadata allocate memory for the raw sample data, fix the
+    // pointer to the raw sample data in the metadata and
+    // read the raw sample data into it.
+    for (int i = 0; i < instrument_data.sample_count; i++)
+    {
+        dumpHexBytes(reinterpret_cast<uint8_t*>(samples_metadata_array + i), sizeof(AudioSynthWavetable::sample_data));
+        Log.verbose("Line %d samples_metadata_array[%d].sample before fixup %x\n", __LINE__, i, samples_metadata_array[i].sample);
+        Log.verbose("Line %d samples_metadata_array[%d].number_of_raw_samples %d\n", __LINE__, i, raw_sample_sizes[i]);
+        // Allocate memory for the raw samples array and fix the pointer
+        int16_t raw_sample_size = raw_sample_sizes[i] * sizeof(uint32_t);
+        if (nullptr == (samples_metadata_array[i].sample = (int16_t*)malloc(raw_sample_size)))
+        {
+            Log.verbose("Line %d Failed to allocate memory for raw samples array\n", __LINE__);
+            free(sample_note_ranges_array);
+            free(samples_metadata_array);
+            for (int j = i -1; j >= 0; j--)
+                free(const_cast<int16_t*>(samples_metadata_array[j].sample));
+            data.close();
+            return nullptr;
+        }
+        Log.verbose("Line %d samples_metadata_array[%d].sample after fixup %x, \n", __LINE__, i, samples_metadata_array[i].sample);
+        updateFilePosition(&data);
+        Log.verbose("Line %d current file position %s\n", __LINE__, filePosition);
+        // Read the raw sample data into the array
+        if (data.read((void*)samples_metadata_array[i].sample, raw_sample_size) != (unsigned int)raw_sample_size)
+        {
+            Log.verbose("Line %d Failed to read raw sample data\n", __LINE__);
+            data.close();
+            
+            free(sample_note_ranges_array);
+            free(samples_metadata_array);
+            return nullptr;
+        }
+        updateFilePosition(&data);
+        Log.verbose("Line %d current file position %s\n", __LINE__, filePosition);
+        dumpHexBytes((uint8_t*)samples_metadata_array[i].sample, 128);
+    }
+    data.close();
+
+    dumpInstrumentData(reinterpret_cast<AudioSynthWavetable::instrument_data*>(&instrument_data));
+    
+    return reinterpret_cast<AudioSynthWavetable::instrument_data*>(&instrument_data);   
+}
---
+++ platformio/src/instrument.h
@@ -0,0 +1,68 @@
+/** \file loop.h
+*/
+#ifndef INSTRUMENT_H
+#define INSTRUMENT_H
+#include <Audio.h>
+
+/**
+ * @brief Declaration of CInstrument class.
+*/
+class CInstrument
+{
+public:
+    struct my_instrument_data_t
+    {
+        uint8_t sample_count;
+        uint8_t* sample_note_ranges;
+        AudioSynthWavetable::sample_data* samples;
+    };
+    /** @brief Deleted copy constructor.*/
+    CInstrument(CInstrument &other) = delete;
+
+    /** @brief Deleted operator overload.*/
+    void operator = (const CInstrument &) = delete;
+
+    /** @brief Create a singleton if necessary.
+     * @return A pointer the the singleton.*/
+    static CInstrument *getInstance();
+
+
+    struct AudioSynthWavetable::instrument_data* load(const char* name);
+
+protected:
+    /** @brief constructor.*/
+    CInstrument() {};
+
+    /** @brief Dump hex bytes.
+     * @param[in] bytes Pointer to the memory to be dumped.
+     * @param[in] count Number of bytes to be dumped.
+    */
+    static void dumpHexBytes(uint8_t *bytes, size_t count);
+
+    /** @brief update the current binary file position
+     * @param[in] file a pointer to the File structure to query.
+    */
+    static void updateFilePosition(File* file);
+    
+    /** @brief Dump instrument data to log.
+     * @param[in] instrumentData A pointer to the instrument data to be logged.
+    */
+    void dumpInstrumentData(AudioSynthWavetable::instrument_data *instrumentData);
+
+    /** @brief A pointer to the singleton.*/
+    static CInstrument* singleton;
+    /** @brief A non const version of the instrument_data struct.*/
+    my_instrument_data_t instrument_data =
+    {
+        0,
+        nullptr,
+        nullptr
+    };
+    /** @brief A pointer to the raw sample sizes array*/
+    uint16_t* raw_sample_sizes = nullptr;
+    /** @brief A pointer to the sample note ranges array.*/
+    uint8_t* sample_note_ranges_array = nullptr;
+    /** @brief A pointer to the samples metadata array.*/
+    AudioSynthWavetable::sample_data *samples_metadata_array = nullptr;
+};
+#endif // INSTRUMENT_H
